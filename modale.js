bfb2b4e298668b774bd24b91233a125cea9f16e3
azumuco <skate-m.c@hotmail.fr>
2024-05-07 15:51:28 +0200

    Add files via upload

---
+++ modale.js
@@ -0,0 +1,242 @@
+////////// Modale 1 //////////
+
+// Ouverture & fermeture de la modale //
+
+let modal = null;
+
+function openModal(e) {
+    e.preventDefault();
+    const target = document.querySelector(".modal");
+    // Faire apparaître la fenêtre //
+    target.style.display = "flex";
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", "true");
+    modal = target;
+    // Fermer la modale si on clique sur "X" ou n'importe où sur l'écran (hors modale) //
+    modal.addEventListener("click", closeModal);
+    document.querySelector(".js-modal-close").addEventListener("click", closeModal);
+    document.querySelector(".js-modal-stop").addEventListener("click", stopPropagation);
+}
+
+function openModal2(e) {
+    e.preventDefault();
+    const target = document.querySelector(".modal2");
+    // Faire apparaître la fenêtre //
+    target.style.display = "flex";
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", "true");
+    modal = target;
+    // Fermer la modale si on clique sur "X" ou n'importe où sur l'écran (hors modale) //
+    modal.addEventListener("click", closeModal);
+    document.querySelector(".js-modal-close").addEventListener("click", closeModal);
+    document.querySelector(".js-modal-stop").addEventListener("click", stopPropagation);
+}
+
+function openModal3(e) {
+    e.preventDefault();
+    const target = document.querySelector(".modal3");
+    // Faire apparaître la fenêtre //
+    target.style.display = "flex";
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", "true");
+    modal = target;
+    // Fermer la modale si on clique sur "X" ou n'importe où sur l'écran (hors modale) //
+    modal.addEventListener("click", closeModal);
+    document.querySelector(".js-modal-close").addEventListener("click", closeModal);
+    document.querySelector(".js-modal-stop").addEventListener("click", stopPropagation);
+}
+
+function openModal4(e) {
+    e.preventDefault();
+    const target = document.querySelector(".modal4");
+    // Faire apparaître la fenêtre //
+    target.style.display = "flex";
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", "true");
+    modal = target;
+    // Fermer la modale si on clique sur "X" ou n'importe où sur l'écran (hors modale) //
+    modal.addEventListener("click", closeModal);
+    document.querySelector(".js-modal-close").addEventListener("click", closeModal);
+    document.querySelector(".js-modal-stop").addEventListener("click", stopPropagation);
+}
+
+function openModal5(e) {
+    e.preventDefault();
+    const target = document.querySelector(".modal5");
+    // Faire apparaître la fenêtre //
+    target.style.display = "flex";
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", "true");
+    modal = target;
+    // Fermer la modale si on clique sur "X" ou n'importe où sur l'écran (hors modale) //
+    modal.addEventListener("click", closeModal);
+    document.querySelector(".js-modal-close").addEventListener("click", closeModal);
+    document.querySelector(".js-modal-stop").addEventListener("click", stopPropagation);
+}
+
+function openModal6(e) {
+    e.preventDefault();
+    const target = document.querySelector(".modal6");
+    // Faire apparaître la fenêtre //
+    target.style.display = "flex";
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", "true");
+    modal = target;
+    // Fermer la modale si on clique sur "X" ou n'importe où sur l'écran (hors modale) //
+    modal.addEventListener("click", closeModal);
+    document.querySelector(".js-modal-close").addEventListener("click", closeModal);
+    document.querySelector(".js-modal-stop").addEventListener("click", stopPropagation);
+}
+
+function openModal7(e) {
+    e.preventDefault();
+    const target = document.querySelector(".modal7");
+    // Faire apparaître la fenêtre //
+    target.style.display = "flex";
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", "true");
+    modal = target;
+    // Fermer la modale si on clique sur "X" ou n'importe où sur l'écran (hors modale) //
+    modal.addEventListener("click", closeModal);
+    document.querySelector(".js-modal-close").addEventListener("click", closeModal);
+    document.querySelector(".js-modal-stop").addEventListener("click", stopPropagation);
+}
+
+function openModal8(e) {
+    e.preventDefault();
+    const target = document.querySelector(".modal8");
+    // Faire apparaître la fenêtre //
+    target.style.display = "flex";
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", "true");
+    modal = target;
+    // Fermer la modale si on clique sur "X" ou n'importe où sur l'écran (hors modale) //
+    modal.addEventListener("click", closeModal);
+    document.querySelector(".js-modal-close").addEventListener("click", closeModal);
+    document.querySelector(".js-modal-stop").addEventListener("click", stopPropagation);
+}
+
+function openModal9(e) {
+    e.preventDefault();
+    const target = document.querySelector(".modal9");
+    // Faire apparaître la fenêtre //
+    target.style.display = "flex";
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", "true");
+    modal = target;
+    // Fermer la modale si on clique sur "X" ou n'importe où sur l'écran (hors modale) //
+    modal.addEventListener("click", closeModal);
+    document.querySelector(".js-modal-close").addEventListener("click", closeModal);
+    document.querySelector(".js-modal-stop").addEventListener("click", stopPropagation);
+}
+
+function openModal10(e) {
+    e.preventDefault();
+    const target = document.querySelector(".modal10");
+    // Faire apparaître la fenêtre //
+    target.style.display = "flex";
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", "true");
+    modal = target;
+    // Fermer la modale si on clique sur "X" ou n'importe où sur l'écran (hors modale) //
+    modal.addEventListener("click", closeModal);
+    document.querySelector(".js-modal-close").addEventListener("click", closeModal);
+    document.querySelector(".js-modal-stop").addEventListener("click", stopPropagation);
+}
+
+function openModal11(e) {
+    e.preventDefault();
+    const target = document.querySelector(".modal11");
+    // Faire apparaître la fenêtre //
+    target.style.display = "flex";
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", "true");
+    modal = target;
+    // Fermer la modale si on clique sur "X" ou n'importe où sur l'écran (hors modale) //
+    modal.addEventListener("click", closeModal);
+    document.querySelector(".js-modal-close").addEventListener("click", closeModal);
+    document.querySelector(".js-modal-stop").addEventListener("click", stopPropagation);
+}
+
+function openModal12(e) {
+    e.preventDefault();
+    const target = document.querySelector(".modal12");
+    // Faire apparaître la fenêtre //
+    target.style.display = "flex";
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", "true");
+    modal = target;
+    // Fermer la modale si on clique sur "X" ou n'importe où sur l'écran (hors modale) //
+    modal.addEventListener("click", closeModal);
+    document.querySelector(".js-modal-close").addEventListener("click", closeModal);
+    document.querySelector(".js-modal-stop").addEventListener("click", stopPropagation);
+}
+
+function closeModal(e) {
+    // Fonction inverse pour fermer la modale //
+    if (modal === null) return
+    e.preventDefault();
+    modal.style.display = "none";
+    modal.setAttribute("aria-hidden", "true");
+    modal.removeAttribute("aria-modal");
+    modal.removeEventListener("click", closeModal);
+    document.querySelector(".js-modal-close").removeEventListener("click", closeModal);
+    document.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation);
+    modal = null;
+}
+
+// Fonction permettant d'éviter de fermer la modale si on clique sur la fenêtre //
+
+const stopPropagation = function (e) {
+    e.stopPropagation();
+}
+
+// Affichage de la modale au clic //
+
+document.querySelectorAll(".js-modal").forEach(a => {
+    a.addEventListener("click", openModal);
+})
+
+document.querySelectorAll(".js-modal2").forEach(a => {
+    a.addEventListener("click", openModal2);
+})
+
+document.querySelectorAll(".js-modal3").forEach(a => {
+    a.addEventListener("click", openModal3);
+})
+
+document.querySelectorAll(".js-modal4").forEach(a => {
+    a.addEventListener("click", openModal4);
+})
+
+document.querySelectorAll(".js-modal5").forEach(a => {
+    a.addEventListener("click", openModal5);
+})
+
+document.querySelectorAll(".js-modal6").forEach(a => {
+    a.addEventListener("click", openModal6);
+})
+
+document.querySelectorAll(".js-modal7").forEach(a => {
+    a.addEventListener("click", openModal7);
+})
+
+document.querySelectorAll(".js-modal8").forEach(a => {
+    a.addEventListener("click", openModal8);
+})
+
+document.querySelectorAll(".js-modal9").forEach(a => {
+    a.addEventListener("click", openModal9);
+})
+
+document.querySelectorAll(".js-modal10").forEach(a => {
+    a.addEventListener("click", openModal10);
+})
+
+document.querySelectorAll(".js-modal11").forEach(a => {
+    a.addEventListener("click", openModal11);
+})
+
+document.querySelectorAll(".js-modal12").forEach(a => {
+    a.addEventListener("click", openModal12);
+})
